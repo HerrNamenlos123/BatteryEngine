5d6743e3915a756487c3490eba07426b0886d32c
HerrNamenlos123 <herrnamenlos123@gmail.com>
2021-01-08 11:51:08 +0100

    Removed setting the release date and simplified the version file handling

--- include/Battery/Updater.h
+++ include/Battery/Updater.h
@@ -8,7 +8,7 @@ namespace Battery {
 
 		/// <summary>
 		/// Get the version string, which was saved in the "version" file in the application settings folder.
-		/// The returned string is the first line of the file, which will be what it was previously set to
+		/// The returned string is the content of the file, which will be what it was previously set to
 		/// or the version string from the Github release. When the file can't be found or is empty, nothing ("")
 		/// is returned
 		/// </summary>
@@ -18,19 +18,8 @@ namespace Battery {
 		std::string GetApplicationVersion();
 
 		/// <summary>
-		/// Get the release date string, which was saved in the "version" file in the application settings folder.
-		/// The returned string is the second line of the file, which will be what it was previously set to
-		/// or the Unix timestamp string from the Github release in the field "published_at". 
-		/// When the file can't be found, is empty or does not have a second line, nothing ("") is returned
-		/// </summary>
-		/// <exception cref="Battery::Exception - Thrown when the Engine was not initialized or when the application
-		/// or organization name was not set"></exception>
-		/// <returns>std::string - The second line of the version file or ""</returns>
-		std::string GetApplicationReleaseDate();
-
-		/// <summary>
 		/// Set the application version in the version file in the application settings folder (%appdata%).
-		/// The string will replace the first line of the file or create it if it does not exist.
+		/// The string will replace the file content or create it if it does not exist.
 		/// This version string is used to check for new available versions and generally contains the version
 		/// string from the installed Github release.
 		/// </summary>
@@ -39,18 +28,6 @@ namespace Battery {
 		/// or organization name was not set"></exception>
 		/// <returns>bool - If the file was successfully written or created</returns>
 		bool SetApplicationVersion(const std::string& version);
-
-		/// <summary>
-		/// Set the application release date in the version file in the application settings folder (%appdata%).
-		/// The string will replace the second line of the file or create it if it does not exist.
-		/// This release date string will usually be a Unix timestamp and is used to check for new available versions 
-		/// and generally contains the release date from the installed Github release in the field "published_at".
-		/// </summary>
-		/// <param name="timestamp">- The release date as a Unix timestamp in string format</param>
-		/// <exception cref="Battery::Exception - Thrown when the Engine was not initialized or when the application
-		/// or organization name was not set"></exception>
-		/// <returns>bool - If the file was successfully written or created</returns>
-		bool SetApplicationReleaseDate(const std::string& timestamp);
 
 		/// <summary>
 		/// Check if a new Github release is available and the version tag is different from the currently installed one.
--- src/Updater.cpp
+++ src/Updater.cpp
@@ -18,51 +18,16 @@ namespace Battery {
 			if (Core::GetApplicationName() != "" || Core::GetOrganizationName() != "") {	// A valid name is given
 
 				std::string file = FileUtils::GetAllegroStandardPath(ALLEGRO_USER_DATA_PATH) + BATTERY_VERSION_FILE;
+				FileUtils::File f = FileUtils::LoadFile(file);
 
-				if (FileUtils::FileExists(file)) {	// File exists
-					FileUtils::File f = FileUtils::LoadFile(file);
+				if (!f.fail())
+					return "";
 
-					if (!f.fail()) {	// File was loaded successfully
-						auto fileLines = StringUtils::SplitString(f.str(), '\n');
-
-						if (fileLines.size() > 0) {	// If fileLines contains anything, 
-							return fileLines[0];	// index 0 must be the first line of the file
-						}
-					}
-				}
-			}
-			else {
-				throw Battery::Exception("Neither the Application nor the Organization name was set");
+				// File was loaded successfully
+				return f.str();
 			}
 
-			// File did not exist or could not be read, return nothing
-			return "";
-		}
-
-		std::string GetApplicationReleaseDate() {
-
-			if (Core::GetApplicationName() != "" || Core::GetOrganizationName() != "") {	// A valid name is given
-
-				std::string file = FileUtils::GetAllegroStandardPath(ALLEGRO_USER_DATA_PATH) + BATTERY_VERSION_FILE;
-
-				if (FileUtils::FileExists(file)) {	// File exists
-					FileUtils::File f = FileUtils::LoadFile(file);
-
-					if (!f.fail()) {	// File was loaded successfully
-						auto fileLines = StringUtils::SplitString(f.str(), '\n');
-
-						if (fileLines.size() >= 2) {	// If fileLines contains at least two lines
-							return fileLines[1];		// index 1 must be the second line of the file
-						}
-					}
-				}
-			}
-			else {
-				throw Battery::Exception("Neither the Application nor the Organization name was set");
-			}
-
-			// File did not exist or could not be read, return nothing
-			return "";
+			throw Battery::Exception("Neither the Application nor the Organization name was set");
 		}
 
 		bool SetApplicationVersion(const std::string& version) {
@@ -72,89 +37,11 @@ namespace Battery {
 				std::string file = FileUtils::GetAllegroStandardPath(ALLEGRO_USER_DATA_PATH) + BATTERY_VERSION_FILE;
 				std::cout << file << std::endl;
 
-				if (FileUtils::FileExists(file)) {	// File exists
-					FileUtils::File f = FileUtils::LoadFile(file);
-
-					if (!f.fail()) {	// File was loaded successfully
-						auto fileLines = StringUtils::SplitString(f.str(), '\n');
-
-						if (fileLines.size() > 0) {	// If fileLines contains anything
-							fileLines[0] = version;		// Replace the first line
-						}
-						else {	// If the file was empty, just create a new line
-							fileLines.push_back(version);
-						}
-
-						// Now save the file
-						std::string fileContent = StringUtils::JoinStrings(fileLines, "\n");
-						return FileUtils::SaveFile(file, fileContent);
-					}
-					// The file failed to load, return below
-				}
-				else {	// File did not exist, create a new one
-					std::vector<std::string> fileLines;
-
-					// Simply add a new line to the empty file
-					fileLines.push_back(version);
-
-					// Now save the file
-					std::string fileContent = StringUtils::JoinStrings(fileLines, "\n");
-					std::cout << "saving file" << std::endl;
-					return FileUtils::SaveFile(file, fileContent);
-				}
-			}
-			else {
-				throw Battery::Exception("Neither the Application nor the Organization name was set");
+				// Simply save the file and return success
+				return FileUtils::SaveFile(file, version);
 			}
 
-			// File could not be read
-			return false;
-		}
-
-		bool SetApplicationReleaseDate(const std::string& timestamp) {
-
-			if (Core::GetApplicationName() != "" || Core::GetOrganizationName() != "") {	// A valid name is given
-
-				std::string file = FileUtils::GetAllegroStandardPath(ALLEGRO_USER_DATA_PATH) + BATTERY_VERSION_FILE;
-
-				if (FileUtils::FileExists(file)) {	// File exists
-					FileUtils::File f = FileUtils::LoadFile(file);
-
-					if (!f.fail()) {	// File was loaded successfully
-						auto fileLines = StringUtils::SplitString(f.str(), '\n');
-
-						if (fileLines.size() >= 2) {	// If fileLines contains at least two lines
-							fileLines[1] = timestamp;		// Replace the second line
-						}
-						else {	// If the file was empty, just create two lines
-							fileLines.push_back("");		// Add an empty line, timestamp must be on the second line
-							fileLines.push_back(timestamp);
-						}
-
-						// Now save the file
-						std::string fileContent = StringUtils::JoinStrings(fileLines, "\n");
-						return FileUtils::SaveFile(file, fileContent);
-					}
-					// The file failed to load, return below
-				}
-				else {	// File did not exist, create a new one
-					std::vector<std::string> fileLines;
-
-					// Simply add the two lines to the empty file
-					fileLines.push_back("");			// Add an empty line, timestamp must be on the second line
-					fileLines.push_back(timestamp);
-
-					// Now save the file
-					std::string fileContent = StringUtils::JoinStrings(fileLines, "\n");
-					return FileUtils::SaveFile(file, fileContent);
-				}
-			}
-			else {
-				throw Battery::Exception("Neither the Application nor the Organization name was set");
-			}
-
-			// File could not be read
-			return false;
+			throw Battery::Exception("Neither the Application nor the Organization name was set");
 		}
 
 
